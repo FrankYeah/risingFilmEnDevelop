5a4c9311ba736b4f67bec279c2aada337d62f8e4
FrankYeah <aaa24295234@gmail.com>
2022-02-13 16:57:33 +0800

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'og:title'  , property: 'og:title'  , content: 'RISING FILMS'},
-      { hid: 'og:image' , property: 'og:image' , content: 'siddharam-logo.webp'},
+      { hid: 'og:image' , property: 'og:image' , content: 'logo.png'},
       { hid: 'og:type' , property: 'og:type' , content: 'website'},
       // { hid: 'og:url' , property: 'og:url' , content: ''},
       { hid: 'description', name: 'description', content: '' },
